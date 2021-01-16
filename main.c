f5dde573f80c2fcaa6910e6dd0b9ac5aea3708f4
SAEED1799 <saeed.esawi99@gmail.com>
2021-01-16 02:41:12 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,33 @@
+#include "trie.h"
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+int main(int argc, char* argv[])
+{
+	
+	bool printInReverse = false;
+	if (argc == 2 && strcmp(argv[1], "r") == 0)
+		printInReverse = true;
+
+	char str[50];//Assuming 50 it's enough for buffersize
+	TrieNode* head = TrieNode_new();
+
+	if (stdin)
+	{
+		while (fscanf(stdin, "%49s", str) == 1)
+		{
+			TrieNode_insert(head,str);
+		}
+		fclose(stdin);
+	}
+	
+
+	str[0] = '\0';
+	TrieNode_print(head, str, sizeof(str), printInReverse);
+
+	TrieNode_delete(head);
+
+	return 0;
+}
+
